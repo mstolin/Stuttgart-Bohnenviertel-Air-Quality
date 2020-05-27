2609c8239d3ab1a135fece3342ec814991bf7832
Marcel Stolin <marcelstolin@gmail.com>
2020-05-27 21:01:51 +0200

    Create first dummy response

---
+++ backend/datasources/Luftdaten.py
@@ -0,0 +1,2 @@
+class Luftdaten:
+    pass
--- backend/resources/AirQuality.py
+++ backend/resources/AirQuality.py
@@ -1,16 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('lat', type=float)
+parser.add_argument('long', type=float)
 
 
 class AirQuality(Resource):
     def get(self, city):
-        if (city == 'stuttgart'):
-            return {
-                'pm10': 10,
-                'o3': 10,
-                'so2': 10,
-                'co': 10,
-                'no': 10,
-                'luqx': 3
-            }
-        else:
-            return {'pm10': 3, 'o3': 3, 'so2': 3, 'co': 3, 'no': 3, 'luqx': 1}
+        args = parser.parse_args()
+        return args
