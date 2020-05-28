4420b4f57a9621a17c3f72a7e17659e4f59c974a
Marcel Stolin <marcelstolin@gmail.com>
2020-05-28 12:08:57 +0200

    Create api endpoint properties

--- backend/datasources/WorldAirQualityProject.py
+++ backend/datasources/WorldAirQualityProject.py
@@ -1,9 +1,19 @@
 import datasources.DataSource as DataSource
+from os import environ
 
 
 class WorldAirQualityProject(DataSource):
-    _API_KEY = ''
-    _URL = 'https://api.waqi.info/feed/'  # geo:10.3;20.7/?token=
+    @property
+    def _api_endpoint_url(self):
+        return 'https://api.waqi.info/feed'
+
+    @property
+    def _api_url(self, lat, lng):
+        token = environ.get('AQODP_API_TOKEN')
+        if (token is None):
+            raise TypeError
+
+        return f'${self._api_endpoint_url}/geo:${lat};${lng}/?token=${token}'
 
     def request_metrics(self):
         pass
