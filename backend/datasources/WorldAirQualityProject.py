c42390bfbee2bd8e3ac98bae457f5885c7dfa682
Marcel Stolin <marcelstolin@gmail.com>
2020-05-28 19:27:54 +0200

    Map api values

--- backend/datasources/WorldAirQualityProject.py
+++ backend/datasources/WorldAirQualityProject.py
@@ -15,12 +15,34 @@ class WorldAirQualityProject(DataSource):
         return f'{self._api_endpoint_url}/geo:{lat};{lng}/?token={token}'
 
     def _map_json_response(self, response):
-        quality_data = response['data']['iaqi']
-        pm10 = quality_data['pm10']['v']
-        o3 = quality_data['o3']['v']
-        no2 = quality_data['no2']['v']
+        result = {'metrics': {}, 'last_update': {}}
 
-        return {'pm10': pm10, 'o3': o3, 'no2': no2}
+        quality_data = response['data']['iaqi']
+        result['metrics'] = self._map_metrics(quality_data)
+
+        time_data = response['data']['time']
+        result['last_update'] = self._map_last_update(time_data)
+
+        return result
+
+    def _map_metrics(self, data):
+        metrics = {}
+        if ('pm10' in data):
+            metrics['pm10'] = data['pm10'].get('v', 0.0)
+        if ('o3' in data):
+            metrics['o3'] = data['o3'].get('v', 0.0)
+        if ('no2' in data):
+            metrics['no2'] = data['no2'].get('v', 0.0)
+        if ('t' in data):
+            metrics['temperature'] = data['t'].get('v', 0.0)
+
+        return metrics
+
+    def _map_last_update(self, data):
+        last_update = {}
+        last_update['date'] = data.get('s', '')
+        last_update['timezone'] = data.get('tz', '')
+        return last_update
 
     def request_metrics(self, lat, lng):
         session = FuturesSession()
