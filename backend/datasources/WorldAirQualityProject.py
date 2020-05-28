f1efe8d628bd2516ced5ac4b8b2c1b08cafdb745
Marcel Stolin <marcelstolin@gmail.com>
2020-05-28 19:05:14 +0200

    Use asynchronous requests now

--- backend/datasources/WorldAirQualityProject.py
+++ backend/datasources/WorldAirQualityProject.py
@@ -1,4 +1,4 @@
-import requests
+from requests_futures.sessions import FuturesSession
 from datasources.DataSource import DataSource
 from os import environ
 
@@ -14,7 +14,17 @@ class WorldAirQualityProject(DataSource):
 
         return f'{self._api_endpoint_url}/geo:{lat};{lng}/?token={token}'
 
+    def _map_json_response(self, response):
+        quality_data = response['data']['iaqi']
+        pm10 = quality_data['pm10']['v']
+        o3 = quality_data['o3']['v']
+        no2 = quality_data['no2']['v']
+
+        return {'pm10': pm10, 'o3': o3, 'no2': no2}
+
     def request_metrics(self, lat, lng):
-        response = requests.get(self._get_api_url(lat, lng))
-        json_response = response.json()
-        return json_response['data']['iaqi']
+        session = FuturesSession()
+        api_future = session.get(self._get_api_url(lat, lng))
+        api_json_response = api_future.result().json()
+
+        return self._map_json_response(api_json_response)
