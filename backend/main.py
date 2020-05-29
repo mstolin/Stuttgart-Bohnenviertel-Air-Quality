611e2f5f46a71e96e9d490c6081ea418d2fe5ab0
Marcel Stolin <marcelstolin@gmail.com>
2020-05-29 18:08:52 +0200

    Activate CORS

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from models.ApiResources import AirQuality
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)  # Plz dont do this in production
 
 api.add_resource(AirQuality, '/airquality')
 
